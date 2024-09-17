c18448898f3f123b75973bb03175a6cde610c4be
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2024-09-16 20:59:34 -0700

    Update init.py

--- init.py
+++ init.py
@@ -1,19 +1,18 @@
 import subprocess
 import time
 
-def open_terminals(nodes_num, base_port):
+def open_replica_terminals(nodes_num, base_port, max_faulty_nodes):
     for i in range(0, nodes_num):
-        command = f"python3 node.py {nodes_num} {base_port} {i}"
+        command = f"python3 node.py {nodes_num} {base_port} {i} {max_faulty_nodes}"
         subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
-def open_client_terminal(nodes_num, base_port):
-    command = f"python3 client.py 1 {nodes_num} {base_port}"
+def open_client_terminal(nodes_num, base_port, max_faulty_nodes):
+    command = f"python3 client.py 1 {nodes_num} {base_port} {max_faulty_nodes}"
     subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
-nodes_num = 4
-base_port = 5045
-open_terminals(nodes_num, base_port)
+max_faulty_nodes = 1
+nodes_num = 3 * max_faulty_nodes + 1
+base_port = 5055
+open_replica_terminals(nodes_num, base_port, max_faulty_nodes)
 time.sleep(5)
-open_client_terminal(nodes_num, base_port)
-
-
+open_client_terminal(nodes_num, base_port, max_faulty_nodes)
