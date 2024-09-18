76883367827719cb2376372d418d474ab02d7be5
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2024-09-17 21:19:50 -0700

    Update init.py

--- init.py
+++ init.py
@@ -4,11 +4,13 @@ import time
 def open_replica_terminals(nodes_num, base_port, max_faulty_nodes):
     for i in range(0, nodes_num):
         command = f"python3 node.py {nodes_num} {base_port} {i} {max_faulty_nodes}"
-        subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
+        subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
+        #subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
 def open_client_terminal(nodes_num, base_port, max_faulty_nodes):
     command = f"python3 client.py 1 {nodes_num} {base_port} {max_faulty_nodes}"
-    subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
+    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
+    #subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
 max_faulty_nodes = 1
 nodes_num = 3 * max_faulty_nodes + 1
